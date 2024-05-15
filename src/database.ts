089afb170ec01cda17e68976ce409ae31d5ccb59
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-15 17:28:53 -0300

    feat: created connection with database

---
+++ src/database.ts
@@ -0,0 +1,12 @@
+import { knex as setupKnes } from 'knex'
+
+export const knex = setupKnes({
+  client: 'mysql2',
+  connection: {
+    host: '127.0.0.2',
+    port: 3306,
+    user: 'root',
+    password: 'Root1234',
+    database: 'simbiteste',
+  },
+})
