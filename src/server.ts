6b2933cfe5382115f3c08648d90ae69df8145ade
Hítalo Rodrigues <hitalo005@gmail.com>
2024-05-15 17:07:30 -0300

    feat: created query sql with knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,33 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello world!'
+app.get('/dados', async (request, reply) => {
+  try {
+    const rows = await knex('tb_teste').select(
+      'id_projeto',
+      'nome',
+      'municipio',
+      'uf',
+      'valor_captado',
+      'valor_aprovado',
+    )
+
+    const result = rows.map((row) => ({
+      id_projeto: row.id_projeto,
+      nome: row.nome,
+      municipio: row.municipio,
+      uf: row.uf,
+      valor_captado: row.valor_captado,
+      valor_aprovado: row.valor_aprovado,
+    }))
+
+    reply.type('application/json').send(JSON.stringify(result))
+  } catch (error) {
+    app.log.error(error)
+    reply.status(500).send({ error: 'Erro ao obter os dados' })
+  }
 })
 
 app.listen({ port: 3333 }).then(() => {
